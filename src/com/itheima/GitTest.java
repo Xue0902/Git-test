3e1ed6dc12ba27939b1c013b50c9cada25e00c56
Four <568862976@qq.com>
2019-06-30 10:34:49 +0800

    提交到本地库

---
+++ src/com/itheima/GitTest.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("第一次提交..");
+
+        test();
+    }
+
+    public static void test (){
+        String arr[] = {"张三","李四"};
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+
+    }
+}
